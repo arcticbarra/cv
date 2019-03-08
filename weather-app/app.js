4dfb2e37a537b7a9fbf51646cb95baa8b17c6a9a
Enrique Barragan <ebarragan1997@gmail.com>
2019-03-07 22:05:59 -0600

    use promises, change log of weather

--- weather-app/app.js
+++ weather-app/app.js
@@ -7,16 +7,36 @@ if (process.argv.length === 3) {
   city_name = process.argv[2];
 }
 console.log(`Cómo está el día en ${city_name}:`);
-request(`https://api.mapbox.com/geocoding/v5/mapbox.places/${city_name}.json?access_token=${creds.MAPBOX_TOKEN}&limit=1`,
-  function (error, response, body) {
-    const parsedBody = JSON.parse(body);
-    const longitude = parsedBody.features[0].center[0];
-    const latitude = parsedBody.features[0].center[1];
-    request(`https://api.darksky.net/forecast/${creds.DARK_SKY_SECRET_KEY}/${latitude},${longitude}?units=si&lang=es`,
+
+
+function getWeather(longitude, latitude) {
+  request(`https://api.darksky.net/forecast/${creds.DARK_SKY_SECRET_KEY}/${latitude},${longitude}?units=si&lang=es`,
+    function (error, response, body) {
+      const parsedBody = JSON.parse(body);
+      const dailyData = parsedBody.daily.data[0];
+      console.log(`${dailyData.summary} Actualmente está a ${parsedBody.currently.temperature}°C. Hay ${dailyData.precipProbability * 100}% de posibilidad de lluvia.`);
+    }
+  )
+}
+
+function getLocation(city_name) {
+  return new Promise(function (resolve, reject) {
+    request(`https://api.mapbox.com/geocoding/v5/mapbox.places/${city_name}.json?access_token=${creds.MAPBOX_TOKEN}&limit=1`,
       function (error, response, body) {
-        const parsedBody = JSON.parse(body);
-        console.log(parsedBody.daily.summary);
+        if (error) {
+          return reject(error);
+        } else {
+          try {
+            const parsedBody = JSON.parse(body);
+            resolve(parsedBody.features[0].center);
+          } catch (e) {
+            reject(e)
+          }
+        }
       }
-    )
-  }
-);
+    );
+  })
+}
+
+
+getLocation(city_name).then((coords) => (getWeather(coords[0], coords[1]))).catch((err) => (console.log(err)));
